f3287c2356973ee34c8dfbf7d1acd470d1183682
Rudo77719 <rudomamyan96@gmail.com>
2022-08-28 20:13:08 +0400

    pagination

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useMemo, useEffect} from "react";
+import React, {useState, useEffect} from "react";
 import './style/App.css'
 import { MyModal } from "./components/MyModal/MyModal";
 import { PostFilter } from "./components/PostFilter";
@@ -6,15 +6,27 @@ import { PostForm } from "./components/PostForm";
 import { PostList } from "./components/PostList";
 import { MyButton } from "./components/ui/button/MyButton";
 import { usePosts } from "./hooks/usePosts";
-import axios from "axios";
 import PostService from "./Api/postService";
+import { Loader } from "./components/ui/loader/Loader";
+import { useFetching } from "./hooks/useFetching";
+import { getPageCount, getPagesArray } from "./utils/pages";
 
 function App() {
-  const [posts, setPosts] = useState([]);
-  const [filter, setFilter] = useState({sort: '', query: ''})
-  const [modal, setModal] = useState(false)
-
+  const [ posts, setPosts ] = useState([]);
+  const [ modal, setModal ] = useState(false);
+  const [ totalPages, setTotalPages ] = useState(0);
+  const [ limit, setLimit ] = useState(10);
+  const [ page, setPage ] = useState()
+  const [ filter, setFilter ] = useState({sort: '', query: ''})
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  let pagesArray = getPagesArray(totalPages) 
+
+  const [ fetchPosts, isLoading, error ] = useFetching(async() => {
+    const response = await PostService.getAll(limit, page)
+    setPosts(response.data);
+    const totalCount = response.headers['x-total-count'];
+    setTotalPages(getPageCount(totalCount, limit));
+  })
 
   useEffect(() => {
     fetchPosts()
@@ -23,11 +35,6 @@ function App() {
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
     setModal(false)
-  }
-
-  async function fetchPosts() {
-    const posts = await PostService.getAll()
-    setPosts(posts)
   }
 
   const removePost = (remove) => {
@@ -48,7 +55,21 @@ function App() {
         filter={filter}
         setFilter={setFilter}
       />
-      <PostList posts={sortedAndSearchedPosts} removePost={removePost} title={'Posts About JS...'}/>
+      {
+        error && <h1>error ${error}</h1>
+      }
+      {
+        isLoading 
+          ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
+          : <PostList posts={sortedAndSearchedPosts} removePost={removePost} title={'Posts About JS...'}/>
+      }
+      <div className="page__wrapper">
+      {
+        pagesArray.map(p => 
+          <span className="page">{p}</span>
+        )
+      }
+      </div>
     </div>
      
   );
