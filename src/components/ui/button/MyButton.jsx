66d979490e62dac580a797afd3d16c6b58464c19
Rudo77719 <rudomamyan96@gmail.com>
2022-10-02 14:26:18 +0400

    pagination

--- src/components/ui/button/MyButton.jsx
+++ src/components/ui/button/MyButton.jsx
@@ -2,9 +2,8 @@ import React from 'react'
 import classes from './MyButton.module.css'
 
 export const MyButton = ({children, ...props}) => {
-  return (
-    <button {...props} className={classes.myBtn}>
-        {children}
-    </button>
-  )
+	return (
+		<button {...props} className={classes.myBtn}>
+			{children}
+		</button>)
 }
--- src/components/ui/navBar/Navbar.jsx
+++ src/components/ui/navBar/Navbar.jsx
@@ -2,14 +2,12 @@ import {Link} from "react-router-dom";
 import React from "react";
 
 export const Navbar = () => {
-	return (
-		<div>
-			<div className='navbar'>
-				<div className='navbar-items'>
-					<Link to='./about'>about site</Link>
-					<Link to='./posts'> posts</Link>
-				</div>
+	return (<div>
+		<div className='navbar'>
+			<div className='navbar-items'>
+				<Link to='./about'>about site</Link>
+				<Link to='./posts'> posts</Link>
 			</div>
 		</div>
-	)
+	</div>)
 }
